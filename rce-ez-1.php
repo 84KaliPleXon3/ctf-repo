5e59f938fd2bc63ecf4182e0153f52002a0d9268
Abdi Prawira N <wearemasskill@outlook.com>
2019-03-16 14:14:26 +0700

    Update rce-ez-1.php

--- rce-ez-1.php
+++ rce-ez-1.php
@@ -1,7 +1,9 @@
-{
-    "version": 2,
-    "name": "ctf-repo",
-    "builds": [
-        { "src": "*.php", "use": "@now/php" }
-    ]
+<?php
+/* Ketika aku melihat mu, aku sange mppshhh sayang */
+if (preg_match("/(data|http|https|ftp):\/\/([a-zA-ZO-9-_]*)\.([a-zA-ZO-9-_]*)/i",$_GET['wannabe'])) {
+die("Huh !? trying to hack me? fck u dumb ass, noob.");
 }
+include($_GET['wannabe']);
+?>
+
+<H2> JUST HACK ME NOOOBBBBB </H2>

1be7eda9afd21955cf85b68c001e5fa92bab8617
Abdi Prawira N <wearemasskill@outlook.com>
2019-03-16 14:25:45 +0700

    Update rce-ez-1.php

--- rce-ez-1.php
+++ rce-ez-1.php
@@ -1,6 +1,6 @@
 <?php
 /* Ketika aku melihat mu, aku sange mppshhh sayang */
-if (preg_match("/(data|http|https|ftp):\/\/([a-zA-ZO-9-_]*)\.([a-zA-ZO-9-_]*)/i",$_GET['wannabe'])) {
+if (preg_match("/(data|http|https|ftp):\/\/([a-zA-Z0-9-_]*)\.([a-zA-Z0-9-_]*)/i",$_GET['wannabe'])) {
 die("Huh !? trying to hack me? fck u dumb ass, noob.");
 }
 include($_GET['wannabe']);
